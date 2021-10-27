ebfcc187189c3c66a251c145f4790ae4786d5a00
PYJS123 <bibi.abrah@gmail.com>
2021-10-27 12:52:47 +0100

    Main players finished (phew)

--- code/public/player.js
+++ code/public/player.js
@@ -1,25 +1,63 @@
 class Player {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.speed = 0;
-    this.acc = 0;
-    this.kpr = false;
+    this.actual = createVector(x, y);
+    this.shown = createVector(x, y);
+    this.speed = {
+      x: 0,
+      y: 0
+    };
+    this.acc = {
+      x: 0,
+      y: 0
+    };
+    // this.kpr = false;
+    this.kpr = {
+      h: false,
+      v: 0       // v can be -1, 0, or 1
+    }
+    this.colour = color(168, 94, 50);
+    this.width = 300;
+    this.height = 100;
+    this.alive = true;
+  }
+
+  overlaps(plr) {
+    if ((abs(plr.actual.x - this.actual.x) < this.width) && ((abs(plr.actual.y - this.actual.y) < this.height))) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   show() {
-    fill(168, 94, 50);
+    fill(this.colour);
     rectMode(CENTER);
     strokeWeight(0);
-    rect(this.x, this.y, 300, 120);
+    rect(this.shown.x, this.shown.y, this.width, this.height);
   }
 
-  update() {
-    this.acc = Math.min(this.acc, 0.5);
-    this.speed += this.acc;
-    this.speed -= 0.2*this.acc;
-    this.speed = Math.min(this.speed, 17);
-    this.speed = Math.max(this.speed, 0);
-    this.x += this.speed;
+  update(height) {
+    // Dealing with x
+    this.acc.x = Math.min(this.acc.x, 0.5);
+    this.speed.x += this.acc.x;
+    this.speed.x -= 0.07;
+    this.speed.x = constrain(this.speed.x, 0, 17);
+    this.actual.x += this.speed.x;
+
+    // Dealing with y
+    if (this.kpr.v == -1) {
+      this.speed.x -= 0.07;
+    } else if (this.kpr.v == 1) {
+      this.speed.x += 0.07;
+    }
+    // this.acc.y = Math.min(this.acc.y, 0.5);
+    // this.acc.y = Math.max(this.acc.y, -0.5);
+    this.acc.y = constrain(this.acc.y, -0.5, 0.5);
+    this.speed.y += this.acc.y;
+    this.speed.y = constrain(this.speed.y, -8, 8);
+    this.speed.y /= 1.7;
+    this.actual.y += this.speed.y;
+    this.actual.y = constrain(this.actual.y, this.height/2, height-(this.height/2));
+    this.shown.y = this.actual.y;
   }
 }
--- code/public/sketch.js
+++ code/public/sketch.js
@@ -1,30 +1,72 @@
 let players = [];
+let playerI;
+let startX = 200;
 
 function setup() {
   createCanvas(windowWidth, windowHeight-5);
   for (let i = 0; i < 5; i++) {
-    players.push(new Player(200, ((height/5)*i)+(height/10)));
+    players.push(new Player(startX, ((height/5)*i)+(height/10)));
   }
+  playerI = floor(random(players.length));
+  players[playerI].colour = color(50, 168, 82);
 }
 
 function draw() {
   background(0, 255, 255);
   for (let i = 0; i < players.length; i++) {
-    players[i].update();
-    players[i].show();
-  }
-
-  if (keyIsPressed) {
-    players[0].kpr = true;
-  } else {
-    players[0].kpr = false;
+    if (players[i].alive) {
+      players[i].update(height);
+      players[i].show();
+    }
   }
 
   for (let i = 0; i < players.length; i++) {
-    if (players[i].kpr) {
-      players[i].acc += 0.04;
-    } else {
-      players[i].acc = 0;
+    let elt = players[i]
+    for (let j = i+1; j < players.length; j++) {
+      if (elt.overlaps(players[j]) == true) {
+        elt.alive = false;
+        players[j].alive = false;
+      }
     }
+  }
+
+  if (keyIsPressed) {
+    if (key.toUpperCase() === 'D' || keyCode === RIGHT_ARROW) {
+      players[playerI].kpr.h = true;
+    } else {
+      players[playerI].kpr.h = false;
+    }
+    if (key.toUpperCase() === 'W' || keyCode === UP_ARROW) {
+      players[playerI].kpr.v = -1;
+    }
+    if (key.toUpperCase() === 'S' || keyCode === DOWN_ARROW) {
+      players[playerI].kpr.v = 1;
+    }
+    if (key.toUpperCase() !== 'W' && key.toUpperCase() !== 'S' && keyCode !== DOWN_ARROW && keyCode !== UP_ARROW) {
+      players[playerI].kpr.v = 0;
+    }
+  } else {
+    players[playerI].kpr.h = false;
+    players[playerI].kpr.v = 0;
+  }
+  for (let i = 0; i < players.length; i++) {
+    if (i == playerI) {
+      continue;
+    }
+    players[i].kpr.h = Boolean(round(random(1)+0.25));
+    // players[i].kpr.v = round(random(-0.8, 0.8));
+  }
+
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].kpr.h) {
+      players[i].acc.x += 0.04;
+    } else {
+      players[i].acc.x = 0;
+    }
+    players[i].acc.y += 0.04*players[i].kpr.v;
+  }
+  for (let i = 0; i < players.length; i++) {
+    let curr = players[i];
+    curr.shown.x = curr.actual.x - players[playerI].actual.x + startX;
   }
 }
